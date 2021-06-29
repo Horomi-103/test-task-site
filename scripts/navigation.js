689941d98a91b34eb283a11fd7b15f6f47eb4a60
Konstantin Barabanov <criitch@yandex.ru>
2021-06-29 17:34:12 +0300

    feat: поддержка кнопки "назад", поддержка нормального href, автоподставка href в goToPage

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,14 @@
-function goToPage(a) {
-  fetch(a)
+// @NOTE: поддержка кнопки браузера "назад"
+window.onpopstate = function(event) {
+  var paths = window.location.pathname.split('/');
+  var pagePath = paths[paths.length - 1]; // достаём последнюю строку из window.location.pathname
+
+  return loadPage(pagePath);
+}
+
+// метод для обновления страницы на новую
+function loadPage(pagePath) {
+  return fetch(pagePath)
     .then(function (answer) {
       if (answer.status == 200) { return answer.text(); }
       else { alert("Ошибка HTTP: " + answer.status); }
@@ -8,8 +17,10 @@ function goToPage(a) {
       var el = document.createElement('html');
       el.innerHTML = result;
       document.querySelector('#content').innerHTML = el.querySelector('#content').innerHTML;
+
+      // обновляем title (@TODO)
       document.querySelector('title').innerHTML = el.querySelector('title').innerHTML;
-      
+
       // @NOTE: переинициализируем все скрипты в #content, потому что иначе они не работают
       // https://www.danielcrabtree.com/blog/25/gotchas-with-dynamically-adding-script-tags-to-html
       document.querySelector('#content').querySelectorAll('script').forEach(oldScript => {
@@ -19,5 +30,22 @@ function goToPage(a) {
         parentElement.removeChild(oldScript);
         parentElement.appendChild(newScript);
       });
+
+      return result;
     })
+}
+
+// метод для перехода на нужную страницу
+function goToPage(pagePath) {
+  if (event) {
+    event.preventDefault(); // отключает переход по ссылке
+
+    if (!pagePath) {
+      pagePath = event.currentTarget.attributes.href.value; // достаём pagePath из href <a>
+    }
+  }
+
+  return loadPage(pagePath).then(() => {
+    history.pushState(null, '', pagePath);
+  });
 }
